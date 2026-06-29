601ff52876ee8f03311fe7b8e299dd025517551f
Roman Yakovlev <romanyakovlev@Romans-MacBook-Air.local>
2026-06-29 18:00:57 +0700

    add exclamation

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     env = os.getenv("ENVIRONMENT", "Local Development")
-    return f"<h1>To-Do Delivery App 🐋</h1><p>Release Status: Live in <b>{env}</b></p>"
+    return f"<h1>To-Do Delivery App 🐋</h1><p>Release Status: Live in <b>{env}!!!</b></p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
