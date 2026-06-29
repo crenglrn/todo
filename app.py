3ad562c590270c68e59d25a85fb8afa382525f62
Roman Yakovlev <romanyakovlev@Romans-MacBook-Air.local>
2026-06-29 17:50:39 +0700

    fixes

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ import os
 
 app = Flask(__name__)
 
-@app.py
+@app.route('/')
 def home():
-    # Grabs a variable from the cloud settings, defaults to "Development"
-    env = os.getenv("ENVIRONMENT", "Development")
-    return f"<h1>To-Do Delivery App 🐋</h1><p>Status: Live in <b>{env}</b></p>"
+    env = os.getenv("ENVIRONMENT", "Local Development")
+    return f"<h1>To-Do Delivery App 🐋</h1><p>Release Status: Live in <b>{env}</b></p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
